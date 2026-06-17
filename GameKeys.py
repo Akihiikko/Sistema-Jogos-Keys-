fdf3e7c4a040c5e93059f08133b7c6b4aecd0aab
Akihiikko <imjaecxy@gmail.com>
2026-06-16 23:50:52 -0300

    Adicionando barra de produto/qtd/valor/id

--- GameKeys.py
+++ GameKeys.py
@@ -6,6 +6,14 @@ print(" ｡ ˚ ︶︶ꔫ︶︶‌ ₊ ˚ ︶︶ꔫ︶︶‌ ｡˚")
 print("  (1) Estoque Disponível \n  (2) Adicionar Item ao Carrinho \n  (3) Ver Carrinho \n  (4) Finalizar Compra \n  (0) Finalizar Programa ૮ ◞ ﻌ ◟ ა")
 print("      ⊹₊˚‧︵‿₊୨ᰔ୧₊‿︵‧˚₊⊹\n")
 
+
+print("          ｡ ˚ ︶︶ꔫ︶︶‌ ₊ ˚ ︶︶ꔫ︶︶‌ ｡˚")
+print("  ID    ||      Produto         ||  Quantidade    || Valor")
+print("          ｡ ˚ ︶︶ꔫ︶︶‌ ₊ ˚ ︶︶ꔫ︶︶‌ ｡˚")
+
+estoque = {"ID", "Preço"}
+print(estoque)
+
 def imprimir_dic(dic, descricao):
     print(f"Estoque Disponível {descricao}")
     for k, v in dic.items():
